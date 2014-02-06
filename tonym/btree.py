3fe5376b63eb8d35a362eac5be01c437376771f4
tonym <tonym@tonym.us>
2014-02-05 23:52:20 -0800

    fix iterNodes() and build()

--- tonym/btree.py
+++ tonym/btree.py
@@ -22,6 +22,8 @@ TODO:
 	* 
 """ 
 import sys
+import logging
+from pprint import pprint,pformat
 __author__ = 'tonym@tonym.us'
 __version__ = '1.0.0' 
 
@@ -44,13 +46,23 @@ class TreeIO:
 	def parse(self, f):
 		"""read file f and build into tree"""
 		# TODO error check on file/bounds/etc
-		tree_list = ','.split(f.readline())
-		subtree_list = ','.split(f.readline())
+		tree_list = f.readline().strip().split(',')
+		subtree_list = f.readline().strip().split(',')
 		return (self.build(tree_list), self.build(subtree_list))
 
 	def build(self, tree_list):
 		""" build tree from ordered list representation"""
-		pass
+		index = Index()
+		root = Node(None, index)
+		root.append(tree_list.pop(0))
+		cur = root
+		while 1:
+			try:
+				cur = cur.append(tree_list.pop(0))
+			except IndexError:
+				break
+		return root
+
 
 
 class Index:
@@ -75,6 +87,9 @@ class Index:
 		except KeyError:
 			# conceal the exception
 			return None
+	
+	def __setitem__(self, k, v):
+		self.add(k,v)
 
 class Node:
 	"""Contains node data and refs to children. Also, ref to index """
@@ -85,31 +100,50 @@ class Node:
 		self.COMPARE_NOT_EQUAL = -0x01
 		# consts for left/right and max index
 		# TODO: make these static members (I forget)
-		self.LEFT =  0x0 
-		self.RIGHT =  0x1 
-		self.MAX_INDEX  = 0x01
-		self.MAX_SIZE   = self.MAX_INDEX + 1
 		
 
 		# index ref shared among nodes
 		self.index = index
-		# children list supports n-ary tree
-		self.children = []
+		self.r = None
+		self.l = None
 		self.data   = data
 
-	def append(self, node):
+	def append(self, data):
 		"""add child left, right. raise error if full"""
-		if(len(self.children) == (self.MAX + 1)):
-			raise InvalidArgumentError
-		else:
-			self.index.add(node.data, node)
-			self.children.append(node)
-	
-	def iterNodes(self, root):
-		if len(self.children) == 0:
-			yield self.data
-		else:
-			yield iter(self.children)
+		if self.data is None:
+			self.data = data
+			self.index[data] = self
+			return self
+		elif self.l is None:
+			self.l = Node(None, self.index)
+			self.l.append(data)
+			return self.l
+		elif self.r is None:
+			self.r = Node(None, self.index)
+			self.r.append(data)
+			return self.r
+
+	def __str__(self):
+		l = []
+		for i in self.iterNodes():
+			l.append(i)
+		return ' '.join(l)
+
+	def iterNodes(self):
+		yield self.data
+		if self.l is not None:
+			for e in self.l.iterNodes():
+				yield e
+		if self.r is not None:
+			for e in self.l.iterNodes():
+				yield e
+
+	def fiterNodes(self,f):
+		f( self.data)
+		if self.l is not None:
+			self.l.fiterNodes(f)
+		if self.r is not None:
+			self.r.fiterNodes(f)
 		
 	def contains(self, qnode):
 		"""look up root node in index. Then recursive compare to children
@@ -124,8 +158,8 @@ class Node:
 
 		# traverse query and target "subtrees" into lists for easy compare
 		# this does cost one extra traversal of the subtree but it's more pythonic
-		qnode_list = [n for n in qnode]
-		tnode_list = [n for n in tnode]
+		qnode_list = [n for n in qnode.iterNodes()]
+		tnode_list = [n for n in tnode.iterNodes()]
 		if qnode_list == tnode_list:
 			return self.COMPARE_EQUAL
 		return self.COMPARE_NOT_EQUAL
